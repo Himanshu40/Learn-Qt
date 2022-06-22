c046bfab4c864999256dc7c6636ddd529b0e1b96
Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>
2022-06-22 11:25:04 +0530

    Added QFIle

---
+++ QFile/widget.cpp
@@ -0,0 +1,103 @@
+#include "widget.h"
+#include "./ui_widget.h"
+#include <QFileDialog>
+#include <QMessageBox>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_readButton_clicked()
+{
+    QString fileContent;
+
+    // Save the file to the disk
+    QString fileName {QFileDialog::getOpenFileName(this, "Open")};
+
+    if (fileName.isEmpty()) {
+        return;
+    }
+
+    QFile file(fileName);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        return;
+    }
+
+    QTextStream in(&file);
+    QString line = in.readLine();
+
+    while (!line.isNull()) {
+        fileContent.append(line);
+        line = in.readLine();
+    }
+
+    file.close();
+    ui->textEdit->clear();
+    ui->textEdit->setPlainText(fileContent);
+}
+
+
+void Widget::on_writeButton_clicked()
+{
+    // Save the file to the disk
+    QString fileName {QFileDialog::getSaveFileName(this, "Save As")};
+
+    if (fileName.isEmpty()) {
+        return;
+    }
+
+    QFile file(fileName);
+
+    // Open the file
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Append)) {
+        return;
+    }
+
+    QTextStream out(&file);
+
+    out << ui->textEdit->toPlainText() << "\n";
+
+    file.close();
+}
+
+
+void Widget::on_copyButton_clicked()
+{
+    QString srcFileName {ui->sourceLineEdit->text()};
+    QString destFileName {ui->destinationLineEdit->text()};
+
+    if (srcFileName.isEmpty() || destFileName.isEmpty()) {
+        return;
+    }
+
+    QFile file(srcFileName);
+
+    if (file.copy(destFileName)) {
+        QMessageBox::information(this, "Success", "Copy Successful");
+    }
+    else {
+        QMessageBox::information(this, "Failure", "Copy failed");
+    }
+}
+
+
+void Widget::on_selectButton_clicked()
+{
+    QString fileName {QFileDialog::getOpenFileName(this, "Choose File")};
+
+    if (fileName.isEmpty()) {
+        return;
+    }
+
+    ui->sourceLineEdit->setText(fileName);
+}
+
